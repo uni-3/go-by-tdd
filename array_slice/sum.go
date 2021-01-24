31579e985c587361864bffd17382de482671a370
uni-3 <yuni.todate@gmail.com>
2021-01-25 07:47:17 +0900

    feat: add sum

--- array_slice/sum.go
+++ array_slice/sum.go
@@ -7,3 +7,25 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+// SumAllTails 最初以外の配列の和を返す
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+			continue
+		}
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+	}
+	return sums
+}
--- array_slice/sum_test.go
+++ array_slice/sum_test.go
@@ -12,7 +12,6 @@ func TestSum(t *testing.T) {
 
 		got := Sum(numbers)
 		expected := 15
-
 		assert.Equal(t, expected, got)
 	})
 
@@ -21,6 +20,40 @@ func TestSum(t *testing.T) {
 
 		got := Sum(numbers)
 		expected := 6
+
+		assert.Equal(t, expected, got)
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("2 arrays", func(t *testing.T) {
+		numbers1 := []int{1, 2}
+		numbers2 := []int{0, 9}
+
+		got := SumAll(numbers1, numbers2)
+		expected := []int{3, 9}
+
+		assert.Equal(t, expected, got)
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("2 arrays", func(t *testing.T) {
+		numbers1 := []int{1, 2}
+		numbers2 := []int{0, 9}
+
+		got := SumAllTails(numbers1, numbers2)
+		expected := []int{2, 9}
+
+		assert.Equal(t, expected, got)
+	})
+
+	t.Run("empty slice", func(t *testing.T) {
+		numbers1 := []int{}
+		numbers2 := []int{3, 4, 5}
+
+		got := SumAllTails(numbers1, numbers2)
+		expected := []int{0, 9}
 
 		assert.Equal(t, expected, got)
 	})
