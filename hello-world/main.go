69f7973884bee5c713383e40ad0e9c7a2b10ab64
uni-3 <yuni.todate@gmail.com>
2021-01-24 23:22:34 +0900

    feat: add spanish lang

--- hello-world/main.go
+++ hello-world/main.go
@@ -2,16 +2,23 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "hello, "
+const spanishHelloPrefix = "hola, "
 
 // domain, available for test
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "world"
 	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name
+	}
+
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", "English"))
 }
--- hello-world/main_test.go
+++ hello-world/main_test.go
@@ -9,15 +9,22 @@ import (
 func TestHello(t *testing.T) {
 
 	t.Run("hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "English")
 		expected := "hello, Chris"
 
 		assert.Equal(t, expected, got)
 	})
 
 	t.Run("when an emply string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "English")
 		expected := "hello, world"
+
+		assert.Equal(t, expected, got)
+	})
+
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("elodie", "Spanish")
+		expected := "hola, elodie"
 
 		assert.Equal(t, expected, got)
 	})
