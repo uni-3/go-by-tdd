0271c177ac321977e893407b9bd4c9ad193679cd
uni-3 <yuni.todate@gmail.com>
2021-01-26 01:46:38 +0900

    feat: use method interface

--- struct_method_interface/shapes_test.go
+++ struct_method_interface/shapes_test.go
@@ -7,15 +7,44 @@ import (
 )
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
-	expected := 40.0
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := Perimeter(rectangle)
+		expected := 40.0
 
-	assert.Equal(t, expected, got)
+		assert.Equal(t, expected, got)
+	})
+
 }
 
 func TestArea(t *testing.T) {
-	got := Area(12.0, 6.0)
-	expected := 72.0
 
-	assert.Equal(t, expected, got)
+	areaTests := []struct {
+		name     string
+		shape    Shape
+		expected float64
+	}{
+		{
+			name:     "rectangle",
+			shape:    Rectangle{12.0, 6.0},
+			expected: 72.0,
+		},
+		{
+			name:     "circle",
+			shape:    Circle{10.0},
+			expected: 314.1592653589793,
+		},
+		{
+			name:     "triangle",
+			shape:    Triangle{12.0, 6.0},
+			expected: 36.0,
+		},
+	}
+
+	for _, test := range areaTests {
+		t.Run(test.name, func(t *testing.T) {
+			got := test.shape.Area()
+			assert.Equal(t, got, test.expected)
+		})
+	}
 }
