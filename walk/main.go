315b715ecf2b435f639186d3edce107bea102209
uni-3 <yuni.todate@gmail.com>
2021-02-12 01:58:48 +0900

    feat: add walk

---
+++ walk/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"reflect"
+)
+
+func walk(x interface{}, fn func(input string)) {
+	val := reflect.ValueOf(x)
+	field := val.Field(0)
+	fn(field.String())
+}
---
+++ walk/main_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+// Test .
+func TestWalk(t *testing.T) {
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			Name: "struct with one string field",
+			Input: struct {
+				Name string
+			}{"chris"},
+			ExpectedCalls: []string{"chris"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v want %v", got, test.ExpectedCalls)
+			}
+
+		})
+	}
+
+}
