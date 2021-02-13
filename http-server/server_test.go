80f7ceb38a96c7dda11283f12c3ceaaa438ef9b9
uni-3 <yuni.todate@gmail.com>
2021-02-14 02:52:44 +0900

    feat: add 404 pattern#

--- http-server/server_test.go
+++ http-server/server_test.go
@@ -17,33 +17,65 @@ func TestGETPlayers(t *testing.T) {
 	}
 	server := &PlayerServer{&store}
 
-	cases := []struct {
-		name     string
-		player   string
-		expected string
-	}{
-		{
-			name:     "return score 20",
-			player:   "Pepper",
-			expected: "20",
-		},
-		{
-			name:     "return score 10",
-			player:   "Floyd",
-			expected: "10",
-		},
-	}
-	for _, test := range cases {
+	t.Run("200", func(t *testing.T) {
+		cases := []struct {
+			name     string
+			player   string
+			expected string
+			status   int
+		}{
+			{
+				name:     "return score 20",
+				player:   "Pepper",
+				expected: "20",
+				status:   200,
+			},
+			{
+				name:     "return score 10",
+				player:   "Floyd",
+				expected: "10",
+				status:   200,
+			},
+		}
+		for _, test := range cases {
 
-		t.Run(test.name, func(t *testing.T) {
-			req := newGetScoreRequest(test.player)
-			res := httptest.NewRecorder()
-			server.ServeHTTP(res, req)
+			t.Run(test.name, func(t *testing.T) {
+				req := newGetScoreRequest(test.player)
+				res := httptest.NewRecorder()
+				server.ServeHTTP(res, req)
 
-			actual := res.Body.String()
-			assert.Equal(t, test.expected, actual)
-		})
-	}
+				assert.Equal(t, res.Code, test.status)
+				assert.Equal(t, test.expected, res.Body.String())
+			})
+		}
+
+	})
+
+	t.Run("404", func(t *testing.T) {
+		cases := []struct {
+			name     string
+			player   string
+			expected string
+			status   int
+		}{
+			{
+				name:     "missing player",
+				player:   "Appolo",
+				expected: "",
+				status:   http.StatusNotFound,
+			},
+		}
+		for _, test := range cases {
+
+			t.Run(test.name, func(t *testing.T) {
+				req := newGetScoreRequest(test.player)
+				res := httptest.NewRecorder()
+				server.ServeHTTP(res, req)
+
+				assert.Equal(t, test.status, res.Code, "check for response status")
+			})
+		}
+	})
 }
 
 func newGetScoreRequest(name string) *http.Request {
